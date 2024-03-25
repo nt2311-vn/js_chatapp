ac960e9c34b4cc621c6fe0e59d5c6d329b8a1d2e
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-25 14:49:23 +0700

    set up socket server on port 3000

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 const ws = require("ws");
+
+const port = "3000";
+const server = new ws.Server({ port: port });
+
+server.on("connection", (socket) => {
+	socket.on("message", (message) => {
+		console.log(message);
+		socket.send(message);
+	});
+});
