aa5ed980698c27e9a74f48de56e75bdedfaad0ff
ntEOS <nguyentoan231196@gmail.com>
2024-03-26 22:04:32 +0700

    complete basic chat app with express and socket.io

--- index.js
+++ index.js
@@ -2,18 +2,32 @@ import express from "express";
 import { createServer } from "http";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	// Log all the request url
 	console.log(req.url);
 	res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+
+	socket.on("chat message", (msg) => {
+		console.log`message: ${msg}`;
+		io.emit("chat message", msg);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
 });
 
 server.listen(3000, () => {
